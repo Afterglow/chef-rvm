dea9e90a7b4a225197d92d4862f72c64496c8f0d
Paul Thomas <paul+github@paulthomas.eu>
2013-05-30 01:19:32 -0400

    rollup of userinstall provider commits

--- providers/userinstall.rb
+++ providers/userinstall.rb
@@ -1,4 +1,11 @@
+class Chef::Provider
+  include Chef::RVM::RecipeHelpers
+  include Chef::RVM::ShellHelpers
+end
+
 action :install do
+
+  script_flags = build_script_flags(new_resource.branch, new_resource.version)
 
   rvmrc_template  :rvm_prefix => new_resource.prefix,
                   :rvm_gem_options => new_resource.gemopts,
--- resources/userinstall.rb
+++ resources/userinstall.rb
@@ -1,7 +1,9 @@
 actions :install
 
 attribute :user, :kind_of => String, :name_attribute => true
-attribute :prefix, :kind_of => String, :default => "#{node['rvm']['user_home_root']}/#{new_resource.user}"
+attribute :prefix, :kind_of => String
 attribute :installer_url, :kind_of => String, :default => "node['rvm']['installer_url']"
 attribute :gemopts, :kind_of => String, :default => node['rvm']['rvm_gem_options']
 attribute :rvmrc, :kind_of => String, :default => node['rvm']['rvmrc']
+attribute :branch, :kind_of => String, :default => node['rvm']['branch']
+attribute :version, :kind_of => String, :default => node['rvm']['version']
